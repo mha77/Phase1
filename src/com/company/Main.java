a7d5e911c01fb8bd775de1a309bdb8ad35894fbf
Markus Häge <haemarkus@gmail.com>
2021-10-09 21:37:07 +0200

    raw interface

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,6 +3,11 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+	    System.out.println("Welcome to Company Lockers Pvt. Ltd.");
+        System.out.println("Markus Häge");
+
+        CLI cli = new CLI();
+        cli.options();
+
     }
 }
